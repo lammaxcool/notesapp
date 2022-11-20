68beb2568fc2580c38f9eaf2bbc811c18b5719ec
lammaxcool <lammaxcool@gmail.com>
2022-11-20 23:04:03 +0200

    Init basic project

---
+++ src/test/java/com/notesapp/notesapp/NotesAppTests.java
@@ -0,0 +1,13 @@
+package com.notesapp.notesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
