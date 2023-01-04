1253fd56f0d0b3644e65da6e223f65726ad7a550
lammaxcool <lammaxcool@gmail.com>
2023-01-04 02:24:25 +0200

    Add missing @Validated annotation

--- notes-app-statisitc/src/main/java/com/notesapp/statistic/kafka/KafkaApplicationProperties.java
+++ notes-app-statisitc/src/main/java/com/notesapp/statistic/kafka/KafkaApplicationProperties.java
@@ -1,9 +1,11 @@
 package com.notesapp.statistic.kafka;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotBlank;
 
+@Validated
 @ConfigurationProperties("application.kafka")
 public record KafkaApplicationProperties(
         @NotBlank String topic,
