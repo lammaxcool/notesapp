350a02a458316a0e149d9144f2c705eb1297aa36
lammaxcool <lammaxcool@gmail.com>
2023-01-15 20:31:00 +0200

    Remove unused note PO

--- notes-app-statisitc/src/main/java/com/notesapp/statistic/model/view/NoteView.java
+++ notes-app-statisitc/src/main/java/com/notesapp/statistic/model/view/NoteView.java
@@ -1,15 +1,10 @@
 package com.notesapp.statistic.model.view;
 
 
-import com.notesapp.statistic.model.persistence.NotePo;
-
 public record NoteView(Long id, String title, String content) {
 
     public NoteView(String title, String content) {
         this(null, title, content);
     }
 
-    public NoteView(NotePo notePo) {
-        this(notePo.getId(), notePo.getTitle(), notePo.getContent());
-    }
 }
