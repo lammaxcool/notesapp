11102c79e435b173b4030106ca43de347e25b175
lammaxcool <lammaxcool@gmail.com>
2023-01-23 00:50:47 +0200

    Add server side throttling with rate limiting

--- notes-app-server/src/main/java/com/notesapp/controller/NotesController.java
+++ notes-app-server/src/main/java/com/notesapp/controller/NotesController.java
@@ -2,9 +2,20 @@ package com.notesapp.controller;
 
 import com.notesapp.model.view.NoteView;
 import com.notesapp.service.NotesService;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -28,6 +39,7 @@ public class NotesController {
         return notesService.getNoteById(id);
     }
 
+    @RateLimiter(name = "rateLimiterGetAll")
     @GetMapping(produces = "application/json")
     public List<NoteView> getAllNotes() {
         return notesService.getAllNotes();
@@ -48,5 +60,10 @@ public class NotesController {
         notesService.deleteNoteById(id);
 
         return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+
+    @ExceptionHandler({RequestNotPermitted.class})
+    @ResponseStatus(HttpStatus.TOO_MANY_REQUESTS)
+    public void handleRequestNotPermitted() {
     }
 }
